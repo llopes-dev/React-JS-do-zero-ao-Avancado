684c352eddb423c35c72a3ef1065a1aa6dca2cf0
Leandro Lopes <leandro.lopes.dev@gmail.com>
2019-12-10 14:51:51 -0300

    switch e iniciando com ES6

---
+++ 2 - Começando no JS/13 - switch.js
@@ -0,0 +1,17 @@
+// switch não é muito usado
+
+x = 1;
+
+switch(x){
+case 0:
+    console.log('O X valoe 0!');
+    break;
+
+case 1:
+    console.log('O X valoe 1!');
+    break;
+
+case 2:
+    console.log('O X valoe 2!');
+    break;
+}
