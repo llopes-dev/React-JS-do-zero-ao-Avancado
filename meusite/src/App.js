5df269bbc8ab563d855cd91fe7e5a05124148c24
Leandro Lopes <leandro.lopes.dev@gmail.com>
2019-12-11 17:04:57 -0300

    Entendendo props e component

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Em Breve!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="mailto:sleandro@leandro.pro"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Contato!
         </a>
       </header>
     </div>
