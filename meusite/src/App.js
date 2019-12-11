ae41c6944fd8575d3c77b6c64ce22513a0fa7024
Leandro Lopes <leandro.lopes.dev@gmail.com>
2019-12-11 19:30:56 -0300

    Entendendo props e component pt2

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -2,25 +2,57 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const Bemvindo = (props) =>{
+const Equipe = (props) => {
+  return (
+  <div>
+    <Sobre nome = {props.nome} cargo = {props.cargo} idade = {props.idade}/>
+    <Social facebook = {props.facebook}/>
+    <hr/>
+  </div>
+  )
+}
+
+const Sobre = (props) => {
   return(
 
     <div>
-  <h2>Bem Vindo {props.nome}</h2>
-  <h3>Tenho {props.idade} de idade</h3>
+    
+    <h2>Olá, sou o {props.nome}</h2>
+    <h3>Cargo: {props.cargo}</h3>
+    <h3>Idade: {props.idade} anos </h3>
+
+    </div>
+  )
+}
+
+const Social = (props) => {
+  return(
+    <div>
+      <a href="https://google.com">Facebook </a>
+      <a>LinkeDin </a>
+      <a>Youtube </a>
     </div>
   );
 }
 
+// App é o componente principal
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Bemvindo nome="Leandro" idade="32"/>
-        <Bemvindo nome= "maria" idade="23"/>>
+      
+        <div>
+          <h1>Conheça Nossa Equipe</h1>
+          <Equipe nome="Leandro" cargo="Programador" idade="32"
+          facebook="https://google.com"/>
+
+          <Equipe nome="maria" cargo="Design" idade="24"
+          facebook="https://google.com"/>
+
+          <Equipe nome="Pedro" cargo="Front" idade="19"
+          facebook="https://google.com"/>
+        </div>
        
-      </header>
+      
     </div>
   );
 }
