fba0d13190fed77bd101171b621317cf6a919a47
Leandro Lopes <leandro.lopes.dev@gmail.com>
2020-02-12 11:20:49 -0300

    ajuste no app

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,60 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const Equipe = (props) => {
-  return (
-  <div>
-    <Sobre nome = {props.nome} cargo = {props.cargo} idade = {props.idade}/>
-    <Social facebook = {props.facebook}/>
-    <hr/>
-  </div>
-  )
+import React, {Component} from 'react';
+
+// Criação do Componente por Class
+class Equipe extends Component{
+  render(){
+    return(
+      <h2>Funcionou</h2>
+    );
+  }
 }
 
-const Sobre = (props) => {
-  return(
+  function App() {
+    return(
+      <div>
+        <h1>Conheça nossa Equipe</h1>
+        <Equipe/>
+      </div>
+    )
+  }
 
-    <div>
-    
-    <h2>Olá, sou o {props.nome}</h2>
-    <h3>Cargo: {props.cargo}</h3>
-    <h3>Idade: {props.idade} anos </h3>
-
-    </div>
-  )
-}
-
-const Social = (props) => {
-  return(
-    <div>
-      <a href="https://google.com">Facebook </a>
-      <a>LinkeDin </a>
-      <a>Youtube </a>
-    </div>
-  );
-}
-
-// App é o componente principal
-function App() {
-  return (
-    <div className="App">
-      
-        <div>
-          <h1>Conheça Nossa Equipe</h1>
-          <Equipe nome="Leandro" cargo="Programador" idade="32"
-          facebook="https://google.com"/>
-
-          <Equipe nome="maria" cargo="Design" idade="24"
-          facebook="https://google.com"/>
-
-          <Equipe nome="Pedro" cargo="Front" idade="19"
-          facebook="https://google.com"/>
-        </div>
-       
-      
-    </div>
-  );
-}
-
-export default App;
+  export default App;
